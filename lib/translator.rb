3b95fc409f5604239463e636b31195bc82aa3d92
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 17:25:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(path)
   # code goes here
   emoticons = YAML.load(File.read(path))
   
-  emoticon_data = emoticons.each_with_object({}) do |(key, value), final_hash|
+  emoticon_data = emoticons.each_with_object({}) do |(key, value), final_array|
   end
  binding.pry
   

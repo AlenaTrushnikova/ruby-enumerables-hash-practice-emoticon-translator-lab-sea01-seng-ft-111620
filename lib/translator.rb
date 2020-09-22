7ec86228a74947e8682d5a86b29d051d7a3a30ae
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 18:28:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,13 +19,8 @@ end
 
 def get_english_meaning(path, emoticon)
   # code goes here
-  emoticons = YAML.load_file(path)
-  emoticon_hash = {}
-  
-  emoticons.each do |english_word, emoticon_set|
-    emoticon_hash[english_word] = {:english => emoticon_set.first, :japanese => emoticon_set.last}
-  end
-    result = emoticon_hash[english_word](emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash[english_word](emoticon)
     if result == nil
       result = "Sorry, that emoticon was not found" 
     end

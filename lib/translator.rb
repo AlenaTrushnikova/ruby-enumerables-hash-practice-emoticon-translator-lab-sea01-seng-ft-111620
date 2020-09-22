caac882079c80053a17ecd4fcd40ee510e321bde
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 18:03:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(path)
   emoticons.each do |english_word, emoticon_set|
     #binding.pry
     
-    emoticon_hash[english_word] = {:english => emoticon_set.first, }
+    emoticon_hash[english_word] = {:english => emoticon_set.first, :japanese => emoticon_set.last}
     
     
     

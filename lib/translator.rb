ca3d3b26ee6c4a33723736772e23361fc37c44ea
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 16:36:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,12 @@ def load_library
   
   emoticon_hash["get_emoticon"] = {}
   emoticon_hash["get_meaning"] = {}
-  
-  
+ 
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  end
+  emoticon_hash  
 end
 
 def get_japanese_emoticon

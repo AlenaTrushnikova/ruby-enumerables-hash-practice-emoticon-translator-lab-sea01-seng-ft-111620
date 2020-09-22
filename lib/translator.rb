b7332fe9e1413342dff68a1a770d1eab4b20b5ab
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 19:01:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,7 @@ def get_english_meaning(path, emoticon)
   # code goes here
   emoticon_hash = load_library(path)
   result = emoticon_hash.key(emoticon)
+  binding.pry
   if result == nil
     result = "Sorry, that emoticon was not found" 
   end

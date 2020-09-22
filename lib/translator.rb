2a142b0d99636393bc4695c7a85832477e53f84f
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-22 18:43:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,9 +22,12 @@ end
 def get_english_meaning(path, emoticon)
   # code goes here
   emoticon_hash = load_library(path)
-  emoticon_hash["get_emoticon"] = Hash.new
-  emoticon_hash["get_meaning"] = Hash.new
   
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+  
+  emoticon_hash["get_meaning"] = Hash.new
+  emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   result = emoticon_hash["get_meaning"][emoticon]
   if result == nil
     result = "Sorry, that emoticon was not found" 
